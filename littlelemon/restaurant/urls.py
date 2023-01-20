28d764e917328921a8481dc929a56da222c709e5
Thais Carvalho <thaisgleite@gmail.com>
2023-01-20 12:33:28 -0300

    serializers

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin 
-from django.urls import path 
+from django.urls import path, include
 from .views import sayHello 
 from . import views
   
 urlpatterns = [ 
     path('hello', sayHello, name='sayHello'), 
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('restaurant/menu/', include('restaurant.urls')),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from .models import MenuItem
+from .serializers import MenuItemSerializer
 
 # Create your views here.
 def sayHello(request):
@@ -7,3 +11,11 @@ def sayHello(request):
 
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(generics.RetrieveAPIView,generics.DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
